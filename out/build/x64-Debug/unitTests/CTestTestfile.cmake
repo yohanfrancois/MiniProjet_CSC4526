64d8fd23ab665acbed8677699a38d14c6f3c83c7
yohanfrancois <124788342+yohanfrancois@users.noreply.github.com>
2024-06-12 15:53:25 +0200

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/unitTests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ETUDES/TSP/3A/CSC4526/MiniProjet/unitTests
-# Build directory: D:/ETUDES/TSP/3A/CSC4526/MiniProjet/out/build/x64-Debug/unitTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/ETUDES/TSP/3A/CSC4526/MiniProjet/out/build/x64-Debug/unitTests/unitTests[1]_include.cmake")
